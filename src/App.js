b61fd02b3ee1aa7a64fca1b4fa2fb96129da6c70
Gamak Grover <you@example.com>
2023-08-26 03:13:58 +0530

    commit

---
+++ src/App.js
@@ -0,0 +1,65 @@
+import axios from "axios";
+import React from "react";
+import "./App.css";
+import { useState } from "react";
+function App() {
+  const [pokemon, Setpokemon] = useState({});
+  const [data, Setdata] = useState("");
+
+  const getData = (pokemon) => {
+    if (!pokemon) return;
+    const URL = `https://pokeapi.co/api/v2/pokemon/${pokemon}`;
+    axios
+      .get(URL)
+      .then((res) => {
+        Setdata(res.data);
+        console.log(res.data);
+      })
+      .catch((err) => {
+        console.log("err", err);
+      });
+  };
+  const handleSearch = () => {
+    getData(pokemon);
+  };
+
+  const handleInput = (e) => {
+    Setpokemon(e.target.value);
+  };
+
+  return (
+    <div>
+      <center>
+        <input id="input" type="text" onChange={handleInput}></input>
+        <br />
+        <button id="search-btn" onClick={handleSearch}>
+          Click
+        </button>
+        <br />
+        <br />
+
+        <img src={data?.sprites?.other?.dream_world?.front_default} alt="img" />
+        <table>
+          <tr>
+            <td>ID</td>
+            <td>{data?.id}</td>
+          </tr>
+          <tr>
+            <td>Name</td>
+            <td>{data?.name}</td>
+          </tr>
+          <tr>
+            <td>Weight</td>
+            <td>{data?.weight / 10}kg</td>
+          </tr>
+          <tr>
+            <td>Height</td>
+            <td>{data?.height / 10}m</td>
+          </tr>
+        </table>
+      </center>
+    </div>
+  );
+}
+
+export default App;
